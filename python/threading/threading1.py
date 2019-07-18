6f6901053f03086c2dc986b2bc7c545c754fd393
lzt <2536733617@qq.com>
2019-07-18 22:51:07 +0800

    threading 2

--- python/threading/threading1.py
+++ python/threading/threading1.py
@@ -1,13 +1,15 @@
 import threading
+import time
 
 def thread_job():
-    print('This is an added Thead,number is %s'% threading.current_thread())
+    print('T1 start\n')
+    for i in range(10):
+        time.sleep(0.1)
+    print('T1 finish\n')
+    
 def main():
     added_thread=threading.Thread(target=thread_job)
     added_thread.start()
-    print(threading.active_count())
-    print(threading.enumerate())
-    print(threading.current_thread())
-
+    print('all done\n')
 if __name__=='__main__':
     main()
