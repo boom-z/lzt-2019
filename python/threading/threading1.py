4eb1479518a7880bb86bbe6e80cb6753ddd94002
lzt <2536733617@qq.com>
2019-07-18 23:02:57 +0800

    threading join

--- python/threading/threading1.py
+++ python/threading/threading1.py
@@ -6,10 +6,17 @@ def thread_job():
     for i in range(10):
         time.sleep(0.1)
     print('T1 finish\n')
-    
+
+def T2_job():
+    print('T2 start\n')
+    print('T2 finish\n')
 def main():
     added_thread=threading.Thread(target=thread_job)
+    thread2=threading.Thread(target=T2_job,name='T2')
     added_thread.start()
+    thread2.start()
+    #added_thread.join()#上面运行完了才能运行下面的
+    #thread2.join()
     print('all done\n')
 if __name__=='__main__':
     main()
